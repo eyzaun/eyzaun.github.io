638c4740497865483b40202d03a87476c85a2ac1
Eyüp Zafer Ünal <eyzaun@gmail.com>
2025-07-21 15:55:14 +0300

    Basitleştirilmiş GitHub Actions workflow

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // username.github.io repoları için root path
+  base: '/', // Repository isminin aynısı olduğu için / kullanabiliriz
   build: {
     outDir: 'dist',
-    sourcemap: false,
-    target: 'esnext',
-    minify: 'terser'
+    sourcemap: false
   }
 })
